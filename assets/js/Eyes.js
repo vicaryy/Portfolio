64370909ed2561f597f206325f6bd3928d8fc3f8
vicary <vicarycholewa@gmail.com>
2024-04-20 18:59:07 +0200

    add cool avatar with eyes following mouse

---
+++ assets/js/Eyes.js
@@ -0,0 +1,42 @@
+const style = document.documentElement.style;
+style.setProperty("--eyesX", "0px");
+
+const leftEye = document.querySelector(".profile-pic .left-eye.dot");
+const leftEy = document.querySelector(".profile-pic .left-eye.dot .left-eye");
+const rightEye = document.querySelector(".profile-pic .right-eye.dot");
+const rightEy = document.querySelector(".profile-pic .right-eye.dot .right-eye");
+
+console.log(leftEye);
+
+
+document.addEventListener('mousemove', function (event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+
+    const test = document.querySelector(".profile-pic");
+    const rect = test.getBoundingClientRect();
+    const testXLeftEye = rect.left + 0 + rect.width / 2; 
+    const testYLeftEye = rect.top + 5 + rect.height / 2;
+
+    const angleDegLeftEye = angle(mouseX, mouseY, testXLeftEye, testYLeftEye);
+
+    leftEye.style.transform = "rotate("+(180 + angleDegLeftEye)+"deg)"
+    leftEy.style.transform = "rotate("+(180 - angleDegLeftEye)+"deg)"
+
+    
+    const testXRightEye = rect.left + 25 + rect.width / 2; 
+    const testYRightEye = rect.top + 5 + rect.height / 2;
+
+    const angleDegRightEye = angle(mouseX, mouseY, testXRightEye, testYRightEye);
+
+    rightEye.style.transform = "rotate("+(180 + angleDegRightEye)+"deg)"
+    rightEy.style.transform = "rotate("+(180 - angleDegRightEye)+"deg)"
+});
+
+function angle(cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg;
+}
